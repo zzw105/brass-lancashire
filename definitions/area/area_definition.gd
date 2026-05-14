bb3c60bbd1ea4c603b16f79a3fe6469920c56387
zhangzewen <zhangzewen@pintechs.com>
2026-05-14 17:20:27 +0800

    添加区域绑定城市

--- definitions/area/area_definition.gd
+++ definitions/area/area_definition.gd
@@ -2,8 +2,14 @@ extends Resource
 class_name AreaDefinition
 ## 区域静态属性
 
-## 唯一ID
+## 区域静态属性-唯一ID
 @export var id: StringName
 
-## 可建造产业
+## 区域静态属性-可建造产业-开发用数据
 @export var can_industry_list: Array[IndustryDefinition] = []
+
+## 区域静态属性-可建造产业Ids
+var can_industry_list_ids: Array[StringName] = []
+
+## 区域静态属性-所属城市Id
+var city_id: StringName = ""
